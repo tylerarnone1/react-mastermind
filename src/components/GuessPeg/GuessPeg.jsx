2e34dfa14c29f62b78189b622224ccfcb0a03b44
Tyler Arnone <tylerarnone@gmail.com>
2019-04-01 09:58:57 -0500

    master

---
+++ src/components/GuessPeg/GuessPeg.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './GuessPeg.module.css'
+const GuessPeg = (props) => (
+  <div className ={styles.GuessPeg}
+  style={{backgroundColor:props.color,
+    border: props.color ? `1px solid ${props.color}`: '1px dashed grey',
+    cursor: props.currentGuess && 'pointer'}}
+    onClick={props.currentGuess ? props.handlePegClick : null}
+  />
+   
+  
+);
+
+export default GuessPeg;
